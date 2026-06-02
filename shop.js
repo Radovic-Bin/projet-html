6ec7a2feda49b18d1aa2f744118cb510b33afb62
Radovic-Bin <radovicbin@gmail.com>
2026-06-02 07:18:42 +0100

    amélioration

--- shop.js
+++ shop.js
@@ -16,24 +16,35 @@ function page_de_connection() {
 }
 //==========================================================================================
 //============================ chargement des produits et affichage ========================
+ let produits = [];
 function chargerproduit() {
-    const list = document.querySelector("ul");
-    fetch("product.json")
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(post => {
-                list.insertAdjacentHTML('beforeend', `
-                    <li>
-                        ${post.nom}
-                        ${post.prix} XAF
-                        ${post.categorie}
-                        ${post.image}
-                    </li>
-                `);
-            });
-        })
+  const list = document.querySelector("#produits-container");
+
+  fetch("product.json")
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(post => {
+        list.insertAdjacentHTML('beforeend', `
+          <div class="carte-produit">
+            <img src="images/${post.image}" alt="${post.nom}">
+            <div class="carte-info">
+              <h3>${post.nom}</h3>
+              <h3>${post.categorie}</h3>
+              <span class="categorie">${post.categorie}</span>
+              <p class="prix">${post.prix} XAF</p>
+            </div>
+          </div>
+        `);
+      });
+    });
 }
 chargerproduit();
 //==========================================================================================
-//==============================filtre de recherche=========================================
+//==============================barre de recherche=========================================
+document.getElementById("recherchenomProduit").addEventListener("input", function() {
+    const motclé = this.value.toLowerCase();
+    const filtreProducts = produits.filter(product => product.nom.toLowerCase().includes(motclé));
+    afficherResultats(filtreProducts);
+});
 
+chargerproduit();
